ee2f36530c855622a45157084d263f9e25c4a16d
Diogo Ide <diogoide@gmail.com>
2018-05-07 23:13:45 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,7 +2,10 @@ class Project
   attr_accessor :title, :backers
 
   def initialize(title)
-    @title = title 
+    @title = title
     @backers = []
   end
+
+  def add_backer(backer)
+    @backers << backer 
 end
